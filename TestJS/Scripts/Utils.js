fa26c926e0c88193ff621e82c02f2700b944462d
Admin <Admin@W10C>
2021-11-04 19:06:46 +0800

    Add project files.

---
+++ TestJS/Scripts/Utils.js
@@ -0,0 +1,19 @@
+﻿/* 
+ * Title : UCOM Utilities Script
+ * Description: xxxxxxxxxxxxxx
+ * Author: Jerry, John
+ * Version: v0.1
+ */
+function Hello() {
+    alert("Hello!!");
+}
+
+function add(x, y) {
+    //if (typeof x != "number" || typeof y != "number")
+    //    throw "參數必須是數值!!";
+    if (typeof x != "number" || typeof y != "number") {
+        throw { name: "ArgumentError", message: "參數必須是數值!!", stack: "Utils.js" };
+    }
+
+    return x + y;
+}
